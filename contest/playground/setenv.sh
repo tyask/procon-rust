ea9857a9aa32dbf0f4efb2bec214c1e1999f0810
takafumi.yasukochi <takafumi.yasukochi@nssys.co.jp>
2023-09-16 23:51:59 +0900

    update

--- contest/playground/setenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-alias a='../../tools/compete_dt.py src/bin/a.rs'
-alias b='../../tools/compete_dt.py src/bin/b.rs'
--- tools/bashrc.sh
+++ tools/bashrc.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 alias cnew="$(dirname ${BASH_SOURCE[0]})/compete_new.sh"
-alias ctest='cargo compete test'
-alias csub='cargo compete submit --no-watch'
-alias copen='cargo compete open'
+alias t='cargo compete test'
 alias r="$(dirname ${BASH_SOURCE[0]})/cargo_run.sh"
+alias d="compete_dt"
 
+function compete_dt() {
+    if [ -z "$1" ]; then
+        echo "ERROR: You must specify source file name"
+        echo "Ex) d a"
+        return 1
+    fi
+    $(dirname ${BASH_SOURCE[0]})/compete_dt.py src/bin/$1.rs
+}
